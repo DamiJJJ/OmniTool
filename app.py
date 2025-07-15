c2fc382ed1d6fbeb884a9c7e4327e7584fa3ec8a
DamiJJJ <damipl1280@outlook.com>
2025-07-15 18:19:24 +0200

    Implement weather module using OpenWeather API

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
+
+OPENWEATHER_API_KEY = "c58789670bdbb47b3015c68589ed18e3" #os.environ.get("OPENWEATHER_API_KEY")
+
+@app.route('/weather', methods=['GET', 'POST'])
+def weather():
+    weather_data = None
+    error = None
+    if request.method == 'POST':
+        city = request.form.get('city')
+        if city:
+            # Używamy API Current Weather Data
+            url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=metric&lang=pl"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                weather_data = {
+                    'city': data['name'],
+                    'temperature': data['main']['temp'],
+                    'description': data['weather'][0]['description'],
+                    'icon': data['weather'][0]['icon'],
+                    'humidity': data['main']['humidity'],
+                    'wind_speed': data['wind']['speed']
+                }
+            else:
+                error = "Nie udało się pobrać danych pogodowych. Sprawdź nazwę miasta."
+        else:
+            error = "Proszę podać nazwę miasta."
+    return render_template('weather.html', weather_data=weather_data, error=error)
 
 @app.route('/')
 def index():
