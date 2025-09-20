e07a1006707035f49f9a17684b13d198a7df8863
DamiJJJ <damipl1280@outlook.com>
2025-09-20 14:45:26 +0200

    Fix password length issue

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
 
     currency_logs = db.relationship("CurrencyLog", backref="user", lazy=True)
     todos = db.relationship("Todo", backref="user", lazy=True)
